f58942b8b09389787bfaeded1dec99c51d3ed590
Sagleft <sagleft@inbox.ru>
2023-01-17 19:24:12 +0200

    add config

--- structs.go
+++ structs.go
@@ -4,4 +4,22 @@ type bot struct {
 	Config config
 }
 
-type config struct{}
+type config struct {
+	DipTresholdPercent float64        `json:"dipTresholdPercent"`
+	UpwardTrend        percentParam   `json:"upwardTrend"`
+	ProfitPercent      float64        `json:"profitPercent"`
+	StopLoss           percentParam   `json:"stopLoss"`
+	TradePair          string         `json:"tradePair"`
+	Deposit            float64        `json:"deposit"`
+	Exchange           exchangeConfig `json:"exchange"`
+}
+
+type percentParam struct {
+	Value   float64 `json:"percent"`
+	Enabled bool    `json:"enabled"`
+}
+
+type exchangeConfig struct {
+	Pubkey   string `json:"pubkey"`
+	Password string `json:"password"`
+}
