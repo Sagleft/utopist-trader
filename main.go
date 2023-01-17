d080734edf581dc85899bba455fe8c2d5f902a1b
Sagleft <sagleft@inbox.ru>
2023-01-17 22:39:20 +0200

    print colored error log

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"strings"
 	"time"
 
@@ -27,7 +26,8 @@ func main() {
 		b.checkBalance,
 		b.run,
 	); err != nil {
-		log.Fatalln(err)
+		color.Red(err.Error())
+		return
 	}
 
 	swissknife.RunInBackground()
