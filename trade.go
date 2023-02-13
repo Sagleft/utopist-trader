31f4a6830e467f559b076fdaaa6cbfafa1d63535
Sagleft <sagleft@inbox.ru>
2023-02-13 21:57:56 +0200

    add `sendOrder` method

--- trade.go
+++ trade.go
@@ -1,6 +1,10 @@
 package main
 
-import "strings"
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
 
 func (b *bot) resetLap() {
 	b.Lap = lap{}
@@ -13,7 +17,7 @@ func (b *bot) getPairPrice() (float64, error) {
 		return 0, err
 	}
 
-	if b.Config.Strategy == botStrategyBuy {
+	if b.isStrategyBuy() {
 		return pairData.BestAskPrice, nil
 	}
 	return pairData.BestBidPrice, nil
@@ -43,15 +47,55 @@ func (b *bot) getIntervalDepositPercent(currentPrice float64) float64 {
 	return 100 - (currentPrice-minPrice)/intervalMaxDeposit
 }
 
+type order struct {
+	PairSymbol string
+	Qty        float64
+	Price      float64
+}
+
+func (o order) ToString() string {
+	jsonBytes, err := json.Marshal(o)
+	if err != nil {
+		return "{}"
+	}
+	return string(jsonBytes)
+}
+
+func (b *bot) isStrategyBuy() bool {
+	return b.Config.Strategy == botStrategyBuy
+}
+
+func (b *bot) sendOrder(o order) error {
+	var err error
+	if b.isStrategyBuy() {
+		_, err = b.Client.Buy(o.PairSymbol, o.Qty, o.Price)
+	} else {
+		_, err = b.Client.Sell(o.PairSymbol, o.Qty, o.Price)
+	}
+	if err != nil {
+		return fmt.Errorf("send market order %s: %w", o.ToString(), err)
+	}
+	return nil
+}
+
 func (b *bot) checkExchange() error {
 	if b.Lap.IntervalNumber == 0 {
-		// TODO: market buy
+		// market buy
+		if err := b.sendOrder(order{}); err != nil {
+			return err
+		}
+
 		// TODO: place TP
 		return nil
 	}
 
 	// TODO: check TP
 	// if executed -> lap finished
-	// else: cancel TP, market buy
+	// else:
+	//    cancel TP,
+	//    market buy
+	if err := b.sendOrder(order{}); err != nil {
+		return err
+	}
 	return nil
 }
