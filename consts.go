4c79429779a40f8c1da18a23127a0e81efa672b6
Sagleft <sagleft@inbox.ru>
2023-02-20 09:55:46 +0200

    change app title

--- consts.go
+++ consts.go
@@ -4,7 +4,7 @@ const (
 	configPath      = "config.json"
 	botStrategyBuy  = "buy"
 	botStrategySell = "sell"
-	previewTitle    = "by SGL"
+	previewTitle    = "u-trader"
 	previewColor    = "green"
 	donateAddress   = "F50AF5410B1F3F4297043F0E046F205BCBAA76BEC70E936EB0F3AB94BF316804"
 
