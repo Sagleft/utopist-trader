5f084f89564a08af916417d67eae9af54e7fb57c
Sagleft <sagleft@inbox.ru>
2023-02-22 16:08:09 +0200

    update cross-platform build script
    add zip

--- build.sh
+++ build.sh
@@ -7,17 +7,22 @@ do
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
 
-    output_name='build/'bot'_'$GOOS'-'$GOARCH
+    output_name='bot_'$GOOS'-'$GOARCH
+    arch_name=$output_name'.zip'
 
     if [ $GOOS = "windows" ]; then
         output_name+='.exe'
     fi
 
     echo "build for "$GOOS-$GOARCH".."
-    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o 'build/'$output_name
     if [ $? -ne 0 ]; then
         echo 'An error has occurred! Aborting the script execution...'
         exit 1
     fi
+
+    cd build
+    zip -1 $arch_name $output_name 'config.json'
+    cd ..
 
 done
